0fd59911c9da8fc4648316df31bff21506467019
binmex <robin019@hotmail.com>
2024-02-23 18:34:23 -0500

    CRUD client and save reservation

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const cors = require("cors");
+const { ApolloServer } = require("apollo-server-express");
+const { typeDefs } = require("./typeDef");
+const { resolvers } = require("./resolvers");
+
+require("dotenv").config();
+require("./drivers/conect-db");
+
+const app = express();
+
+async function start() {
+  app.use(cors());
+  app.use(express.json());
+  const apolloServer = new ApolloServer({
+    typeDefs,
+    resolvers,
+  });
+
+  await apolloServer.start();
+
+  apolloServer.applyMiddleware({ app });
+
+  app.set("PORT", process.env.PORT || 3000);
+
+  app.use("/", (req, res) =>
+    res.send("Back del proyecto de creación y consumo de APIs")
+  );
+
+  const port = app.get("PORT");
+  app.listen(port, () => console.log(`Server listening on port ${port}`));
+}
+
+start(); // Llamamos a la función start antes de definir las rutas y de iniciar el servidor
---
+++ typeDef.js
@@ -0,0 +1,46 @@
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+
+  type Client {
+    id: ID!
+    name: String!
+    celphone: String
+    email: String
+    reservations: [Reservation]
+  }
+
+  type Reservation {
+    id: ID!
+    bookingStartDate: String!
+    bookingEndDate: String!
+    service: String!
+    comments: String
+    client: Client
+  }
+
+  type Query {
+    hello: String
+    getAllClient: [Client]
+    getClientByID(id: ID!): Client
+  }
+  
+  type Mutation {
+    createClient(id: ID!, name: String!, celphone: String, email: String): Client
+
+    createReservation(
+      id: ID!
+      bookingStartDate: String!
+      bookingEndDate: String!
+      service: String!
+      comments: String
+      idClient: ID!
+    ): Reservation
+
+    deleteClient(id: ID!): Client
+
+    updateClient(id: ID!, name: String, celphone: String, email: String): Client
+  }
+`;
+
+module.exports = { typeDefs };
